40b8af214f21c569ecd5a3d5f30f6157373365b0
Archana <archana@IN-ABRAMEGOWDA-MBP.local>
2020-03-19 00:35:12 +0530

    review and accept page implementation

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 $(document).ready(function($) {
+  $(".sign-up-button").mouseenter(function() {
+    $(".sign-up-button img").addClass("bounce");
+  });
 
-      $('.sign-up-button').mouseenter(function(){
-        $('.sign-up-button img').addClass('bounce');
-      });
+  $(".sign-up-button").mouseleave(function() {
+    $(".sign-up-button img").removeClass("bounce");
+  });
 
-      $('.sign-up-button').mouseleave(function(){
-        $('.sign-up-button img').removeClass('bounce');
-      })
+  $(".accept-continue-btn").mouseenter(function() {
+    $(".accept-continue-btn img").addClass("bounce");
+  });
+
+  $(".accept-continue-btn").mouseleave(function() {
+    $(".accept-continue-btn img").removeClass("bounce");
+  });
+
+  $('#review-tab').show();
+  $('.review-tab').addClass('active');
 });
+
+function openTab(evt, tabID) {
+  stop.evt
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabID).style.display = "block";
+  evt.currentTarget.className += " active";
+}
